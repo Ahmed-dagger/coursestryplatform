628f7851866d2454ed984f29549a3876a7aa0dcd
Ahmed dagger <143612205+Ahmed-dagger@users.noreply.github.com>
2024-09-24 16:17:24 +0300

    teacher name header

--- resources/views/frontend/coursePage.blade.php
+++ resources/views/frontend/coursePage.blade.php
@@ -41,9 +41,9 @@
 
                 <div class="container d-flex mt-3">
                     <img src="../img/Bannerwoman.png" width="30" class="rounded-circle" alt="">
-                    <h6 class="text-light pt-2 pl-3">Teacher</h6>
+                    <h6 class="text-light pt-2 pl-3">{{ $teacher -> name }}</h6>
                     <h6 class="text-light pt-2 pl-3">|</h6>
-                    <h6 class="pt-2 pl-3 text-light">Last Update 26 December 2024</h6>
+                    <h6 class="pt-2 pl-3 text-light">Released At: {{ $course -> created_at }}</h6>
 
                 </div>
                 <div class="container py-4 d-flex mb-3">
